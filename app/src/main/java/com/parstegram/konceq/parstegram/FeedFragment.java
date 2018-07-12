3e0c746d5920be618a3edcc263dc480f3676e8c2
Konce Quispe <konceq@fb.com>
2018-07-12 16:52:29 -0700

    FeedFragment fully functional

--- app/src/main/java/com/parstegram/konceq/parstegram/FeedFragment.java
+++ app/src/main/java/com/parstegram/konceq/parstegram/FeedFragment.java
@@ -2,19 +2,92 @@ package com.parstegram.konceq.parstegram;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FeedFragment extends Fragment {
 
+    ArrayList<Post> posts;
+    RecyclerView feed;
+    PostAdapter postAdapter;
+    private SwipeRefreshLayout swipeContainer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_post, container, false);
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_feed);
+
+        feed = view.findViewById(R.id.feed);
+        posts = new ArrayList<>();
+        postAdapter = new PostAdapter(posts);
+        feed.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        feed.setAdapter(postAdapter);
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        
+        populateTimeline();
+    }
+
+    public void fetchTimelineAsync(){
+        postAdapter.clear();
+        populateTimeline();
+        swipeContainer.setRefreshing(false);
+    }
+
+    public void populateTimeline(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class).include("user");
+        // Execute the find asynchronously
+        query.setLimit(20);
+        query.whereEqualTo("user", ParseUser.getCurrentUser());
+        query.orderByDescending("createdAt");
+        query.findInBackground(new FindCallback<Post>() {
+            public void done(List<Post> itemList, ParseException e) {
+                if (e == null) {
+                    // Access the array of results here
+                    posts.addAll(itemList);
+
+                    postAdapter.notifyDataSetChanged();
+                } else {
+                    Log.d("item", "Error: " + e.getMessage());
+                }
+            }
+        });
     }
 
 }
