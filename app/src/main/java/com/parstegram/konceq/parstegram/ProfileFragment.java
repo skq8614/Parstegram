863e10de37a04762402a9de0fbea32e8a407ab09
Konce Quispe <konceq@fb.com>
2018-07-12 20:47:31 -0700

    Added logout button on profile page

--- app/src/main/java/com/parstegram/konceq/parstegram/ProfileFragment.java
+++ app/src/main/java/com/parstegram/konceq/parstegram/ProfileFragment.java
@@ -1,19 +1,37 @@
 package com.parstegram.konceq.parstegram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 
 public class ProfileFragment extends Fragment {
 
+    private Button logoutButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        logoutButton = view.findViewById(R.id.logoutBtn);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent i = new Intent(ProfileFragment.this.getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
